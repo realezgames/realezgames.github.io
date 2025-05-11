49076a8ff269aca6db6f22bffe83092681da38c2
realezgames <sebastian.z.41782@gmail.com>
2025-05-10 19:36:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+const validCredentials = [
+  {
+    username: 'ez',
+    passwords: ['games', '2900']
+  }
+];
 
-  
-function _0x54b3(_0x55651a,_0x405334){const _0x32df99=_0x32df();return _0x54b3=function(_0x54b3e7,_0x4a1c16){_0x54b3e7=_0x54b3e7-0x16b;let _0x192849=_0x32df99[_0x54b3e7];return _0x192849;},_0x54b3(_0x55651a,_0x405334);}const _0x1f1588=_0x54b3;(function(_0x316dc4,_0x50d3c1){const _0x2dec96=_0x54b3,_0x2a5d71=_0x316dc4();while(!![]){try{const _0x41e972=-parseInt(_0x2dec96(0x17c))/0x1+parseInt(_0x2dec96(0x184))/0x2+parseInt(_0x2dec96(0x185))/0x3+parseInt(_0x2dec96(0x178))/0x4*(-parseInt(_0x2dec96(0x175))/0x5)+-parseInt(_0x2dec96(0x172))/0x6*(parseInt(_0x2dec96(0x182))/0x7)+parseInt(_0x2dec96(0x17a))/0x8*(-parseInt(_0x2dec96(0x16d))/0x9)+-parseInt(_0x2dec96(0x170))/0xa*(-parseInt(_0x2dec96(0x17b))/0xb);if(_0x41e972===_0x50d3c1)break;else _0x2a5d71['push'](_0x2a5d71['shift']());}catch(_0x1649cf){_0x2a5d71['push'](_0x2a5d71['shift']());}}}(_0x32df,0xcd405));function _0x32df(){const _0x15d4d8=['797232HOwnfB','5042796FUOeoi','passwords','play','some','#password','2528289GwmPfR','includes','submit','20NnxAys','block','13788wZIfqe','Error:\x20Invalid\x20username\x20or\x20password','addEventListener','140445IxisXb','preventDefault','none','148nmiwxv','querySelector','8RjhMVb','13512125rXczhD','906953JCZFMI','clicksoundeffect.mp3','style','value','#username','username','4473FjqUbO','games'];_0x32df=function(){return _0x15d4d8;};return _0x32df();}const validCredentials=[{'username':'ez','passwords':[_0x1f1588(0x183),'2900']}],loginForm=document[_0x1f1588(0x179)]('#login-form'),usernameInput=loginForm['querySelector'](_0x1f1588(0x180)),passwordInput=loginForm[_0x1f1588(0x179)](_0x1f1588(0x16c)),content=document['querySelector']('#content');loginForm[_0x1f1588(0x174)](_0x1f1588(0x16f),_0x449317=>{const _0x1635f9=_0x1f1588;_0x449317[_0x1635f9(0x176)]();const _0x553b7f=usernameInput[_0x1635f9(0x17f)],_0x5bcba0=passwordInput[_0x1635f9(0x17f)],_0x1af377=validCredentials[_0x1635f9(0x16b)](_0x2c2ff7=>{const _0x59c907=_0x1635f9;return _0x2c2ff7[_0x59c907(0x181)]===_0x553b7f&&_0x2c2ff7[_0x59c907(0x186)][_0x59c907(0x16e)](_0x5bcba0);});_0x1af377?(content['style']['display']=_0x1635f9(0x171),loginForm[_0x1635f9(0x17e)]['display']=_0x1635f9(0x177),clicksoundeffect[_0x1635f9(0x187)]()):alert(_0x1635f9(0x173));});const clicksoundeffect=new Audio(_0x1f1588(0x17d));
+const loginForm = document.querySelector('#login-form');
+const usernameInput = loginForm.querySelector('#username');
+const passwordInput = loginForm.querySelector('#password');
+const content = document.querySelector('#content');
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const enteredUsername = usernameInput.value;
+  const enteredPassword = passwordInput.value;
+
+  const isValid = validCredentials.some(user =>
+    user.username === enteredUsername && user.passwords.includes(enteredPassword)
+  );
+
+  if (isValid) {
+    content.style.display = 'block';
+    loginForm.style.display = 'none';
+    clicksoundeffect.play();
+  } else {
+    alert('Error: Invalid username or password');
+  }
+});
+
+const clicksoundeffect = new Audio('clicksoundeffect.mp3');
+
+
 
 function cloak(){
   var tab = window.open('about:blank', '_blank');
